10cde2194d3d24cb7bc40c78c2cc4b75063fd5e5
府川和樹 <kazukifukawa1012@gmail.com>
2017-09-22 16:20:59 +0900

    firstcommit

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,38 @@
+class TweetsController < ApplicationController
+ before_action :move_to_index, except: :index
+
+def index
+
+ @tweets = Tweet.all.order("id DESC").page(params[:page]).per(5)
+
+end
+
+def new 
+
+end
+
+
+def create
+	
+
+Tweet.create(tweet_params)
+
+    end
+ 
+ private
+   
+
+     def tweet_params
+
+      params.permit(:name, :image, :text)
+     end
+
+    def move_to_index
+    	
+    	redirect_to action: :index unless user_signed_in?
+    		
+    	end
+
+
+
+end
